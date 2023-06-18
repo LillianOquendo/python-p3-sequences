ef15914d3413d589846ba7dda910434d0a1010fc
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-06-17 21:46:46 -0400

    Python Sequences Lab Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+#adding the laste two numbers to the left and building up to length
+ #if length is 1 then print outputs 0
+ #if length is 2 then print outputs [0,1]
+ #you need to use the .append method to add to the end of your sequence 
+ # we need to subtract 2 from your initial integer and subtract 1 from our secondary integer
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibonacci_sequence = [0, 1]
+        for n in range(2, length):
+            next_number = fibonacci_sequence[n-1] + fibonacci_sequence[n-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
+
